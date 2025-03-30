dd2109c004a69e182e684d31934079524256d933
Suhail Malik <iamdatascientistcg@gmail.com>
2025-03-31 04:15:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 # Load environment variables from .env file
 load_dotenv()
 
-API_KEY=AIzaSyC9fxkI_FBtxtktj9_8auRuiTIXGC7bOqo
-UNSPLASH_ACCESS_KEY=c3k6LDohxj9htpSBybDVzfzWSIVXBZ3Rr4GVY2T_LgQ
-GOOGLE_PLACES_API_KEY=AIzaSyBhhwhq2E8sOvXNlWcvbwYtJyY6WigaBgU
+API_KEY="AIzaSyC9fxkI_FBtxtktj9_8auRuiTIXGC7bOqo"
+UNSPLASH_ACCESS_KEY="c3k6LDohxj9htpSBybDVzfzWSIVXBZ3Rr4GVY2T_LgQ"
+GOOGLE_PLACES_API_KEY="AIzaSyBhhwhq2E8sOvXNlWcvbwYtJyY6WigaBgU"
 
