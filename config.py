8b15fe7fe899fe92e402d7b9c8b9be19c95c9e8f
Suhail Malik <iamdatascientistcg@gmail.com>
2025-03-31 04:13:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ import os
 # Load environment variables from .env file
 load_dotenv()
 
-API_KEY = os.getenv("API_KEY")
-UNSPLASH_ACCESS_KEY = os.getenv("UNSPLASH_ACCESS_KEY")
-GOOGLE_PLACES_API_KEY = os.getenv("GOOGLE_PLACES_API_KEY")
+API_KEY=AIzaSyC9fxkI_FBtxtktj9_8auRuiTIXGC7bOqo
+UNSPLASH_ACCESS_KEY=c3k6LDohxj9htpSBybDVzfzWSIVXBZ3Rr4GVY2T_LgQ
+GOOGLE_PLACES_API_KEY=AIzaSyBhhwhq2E8sOvXNlWcvbwYtJyY6WigaBgU
+
